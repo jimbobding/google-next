3e776ebe431c8ab11ca9f379d41cfe0503d53f39
jimbobding <jimmygrahamdavid@gmail.com>
2024-03-05 17:08:44 +0000

    updated search header compnent

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SearchBox from "./SearchBox";
+import { TbGridDots } from "react-icons/tb";
+import { BsGrid3X3Gap } from "react-icons/bs";
+import { RiSettings3Line } from "react-icons/ri";
 
 export default function SearchHeader() {
-  return <div>SearchHeader</div>;
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center justify-between">
+        <Link href={"https://mail.google.com"} className="hover:underline">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png"
+            alt="Google Logo"
+            width={120}
+            height={40}
+            priority
+            style={{ width: "auto" }}
+          />
+        </Link>
+        <div className="flex-1">
+          <SearchBox />
+        </div>
+        <div className="hidden md:inline-flex space-x-2">
+          <RiSettings3Line className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2 cursor-pointer" />
+          <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2 cursor-pointer" />
+        </div>
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2">
+          Sign In{" "}
+        </button>
+      </div>
+    </header>
+  );
 }
