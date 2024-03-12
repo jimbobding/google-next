ee0f02554502a4434d572d9095783f0eb3f99b93
jimbobding <jimmygrahamdavid@gmail.com>
2024-03-12 14:08:21 +0000

    added pagination to web and image page

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import React from "react";
+import PaginationButtons from "./PaginationButtons";
 
-function ImageSearchResults({ results }) {
+export default function ImageSearchResults({ results }) {
   return (
-    <div className="pb-24 mt-4 ">
-      <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-4">
+    <div className="sm:pb-24 pb-40 mt-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
         {results.items.map((result) => (
           <div className="mb-8" key={result.link}>
             <div className="group">
@@ -12,16 +12,16 @@ function ImageSearchResults({ results }) {
                 <img
                   src={result.link}
                   alt={result.title}
-                  className="h-60 group-hover:shadow-lg transition-shadow duration-300 object-contain cursor-pointer w-full"
+                  className="h-60 group-hover:shadow-xl w-full object-contain transition-shadow duration-300"
                 />
               </Link>
               <Link href={result.image.contextLink}>
-                <h2 className="group-hover:underline truncate test-xl">
+                <h2 className="group-hover:underline truncate text-xl">
                   {result.title}
                 </h2>
               </Link>
               <Link href={result.image.contextLink}>
-                <p className="group-hover:underline truncate test-gray-600">
+                <p className="group-hover:underline truncate text-gray-600">
                   {result.displayLink}
                 </p>
               </Link>
@@ -29,8 +29,9 @@ function ImageSearchResults({ results }) {
           </div>
         ))}
       </div>
+      <div className="ml-16">
+        <PaginationButtons />
+      </div>
     </div>
   );
 }
-
-export default ImageSearchResults;
