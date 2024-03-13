bf08dd4fd41991b75e3f16d1c24e10ffd159a9ae
jimbobding <jimmygrahamdavid@gmail.com>
2024-03-13 17:00:36 +0000

    test

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -15,6 +15,7 @@ export default function WebSearchPage({ searchParams }) {
 async function WebSearchContent({ searchParams }) {
   const startIndex = searchParams.start || "1";
   await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
   );
